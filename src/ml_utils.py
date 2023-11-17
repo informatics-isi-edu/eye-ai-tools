5b95d70dcbca5334acdb910d5c6d6fc1552528f3
Carl Kesselman <carl@isi.edu>
2023-11-16 17:27:04 -0800

    Added function to check for vocabulary tables;.

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -1,6 +1,9 @@
 import deriva.core.utils.globus_auth_utils
 from deriva.core import ErmrestCatalog, HatracStore, AttrDict, get_credential
+import deriva.core.ermrest_model as ermrest_model
 import deriva.core.datapath as datapath
+import pandas as pd
+
 from typing import List
 import logging
 from deriva.core import init_logging
@@ -21,7 +24,7 @@ class EyeAI():
     - __init__(self, protocol: str, hostname: str, catalog_number: str): Initializes the HelperRoutines object.
     - create_new_vocab(self, schema_name: str, table_name: str, name: str, description: str, synonyms: List[str] = [], exist_ok: bool = False) -> str: Creates a new tag in the catalog.
     """
-    def __init__(self, hostname: str = 'eye-ai.org', catalog_number: str = 'eye-ai'):
+    def __init__(self, hostname: str = 'www.eye-ai.org', catalog_number: str = 'eye-ai'):
         """
         Initializes the HelperRoutines object.
 
@@ -36,6 +39,15 @@ class EyeAI():
         self.pb = self.catalog.getPathBuilder()
         self.eye_ai = self.pb.schemas['eye-ai']
 
+    def _vocab_columns(self, table: ermrest_model.Table):
+        vocab_columns = {'Name', 'URI', 'Synonyms', 'Description', 'ID'}
+        def is_vocab(table: ermrest_model.Table):
+            return vocab_columns.issubset({c.name for c in table.columns})
+
+        return [fk.columns[0].name for fk in table.foreign_keys
+                if len(fk.columns) == 1 and is_vocab(fk.pk_table)]
+
     @property
     def image(self) -> datapath._TableWrapper:
         return self.eye_ai.Image
+
